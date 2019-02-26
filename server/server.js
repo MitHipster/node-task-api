18dc9b0d63d56742f359e3dd3dfb72a4a888e823
Tim Acker <tjacker31@gmail.com>
2019-02-25 21:35:27 -0500

    Refactors mongoose connection out of server.js to separate file

--- server/server.js
+++ server/server.js
@@ -1,34 +1,3 @@
-const mongoose = require('mongoose');
-const Todo = require('../model/Todo');
-const User = require('../model/User');
-
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
-
-const newTodo = new Todo({
-	text: 'Learn Angular',
-	completed: false,
-	completedAt: 0
-});
-
-newTodo.save().then(
-	doc => {
-		console.info('Saved todo', doc);
-	},
-	err => {
-		console.warn('Unable to save todo.', err);
-	}
-);
-
-const newUser = new User({
-	email: 'tim@example.com'
-});
-
-newUser.save().then(
-	doc => {
-		console.info('Saved user', doc);
-	},
-	err => {
-		console.warn('Unable to save user.', err);
-	}
-);
+const mongoose = require('./db/mongoose');
+const Todo = require('../models/Todo');
+const User = require('../models/User');
