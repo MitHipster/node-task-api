63ee6edf48363713fcba83accef455432241ae40
Tim Acker <tjacker31@gmail.com>
2019-02-27 20:55:22 -0500

    Adds tests for get route by Id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,18 +1,22 @@
 /* global beforeEach describe it */
 const expect = require('expect');
 const request = require('supertest');
+const { ObjectID } = require('mongodb');
 
 const app = require('../server');
 const Todo = require('../../models/Todo');
 
 const todos = [
 	{
+		_id: new ObjectID(),
 		text: 'First todo test'
 	},
 	{
+		_id: new ObjectID(),
 		text: 'Second todo test'
 	},
 	{
+		_id: new ObjectID(),
 		text: 'Third todo test'
 	}
 ];
@@ -79,6 +83,32 @@ describe('GET /todos', () => {
 			.expect(res => {
 				expect(res.body.todos.length).toBe(3);
 			})
+			.end(done);
+	});
+});
+
+describe('GET /todos/:id', () => {
+	it('should return todo document', done => {
+		request(app)
+			.get(`/todos/${todos[1]._id.toHexString()}`)
+			.expect(200)
+			.expect(res => {
+				expect(res.body.todo.text).toBe(todos[1].text);
+			})
+			.end(done);
+	});
+
+	it('should return 404 if todo not found', done => {
+		request(app)
+			.get(`/todos/${new ObjectID().toHexString()}`)
+			.expect(404)
+			.end(done);
+	});
+
+	it('should return 404 if Id is invalid', done => {
+		request(app)
+			.get('/todos/12345')
+			.expect(404)
 			.end(done);
 	});
 });
