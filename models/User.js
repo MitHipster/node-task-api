87b921040a8b229d2e6bb78cbd5d09aad0fff322
Tim Acker <tjacker31@gmail.com>
2019-03-18 21:11:09 -0400

    Adds name and password fields to User model and includes validator for email validation

--- models/User.js
+++ models/User.js
@@ -1,11 +1,35 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const User = mongoose.model('User', {
-	email: {
+	name: {
 		type: String,
 		required: true,
 		minlength: 1,
 		trim: true
+	},
+	email: {
+		type: String,
+		required: true,
+		minlength: 1,
+		trim: true,
+		lowercase: true,
+		validate(value) {
+			if (!validator.isEmail(value)) {
+				throw new Error('Please provide a valid email address.');
+			}
+		}
+	},
+	password: {
+		type: String,
+		required: true,
+		minlength: 7,
+		trim: true,
+		validate(value) {
+			if (value.toLowerCase().includes('password')) {
+				throw new Error('Your password cannot contain "password".');
+			}
+		}
 	}
 });
 
