f489226e7a909cadb13ed5b00fa4393e14f6d1db
Tim Acker <tjacker31@gmail.com>
2019-04-12 21:35:50 -0400

    Adds authentication to get all tasks and task by id

--- src/routers/task.js
+++ src/routers/task.js
@@ -19,21 +19,35 @@ router.post('/tasks', auth, async (req, res) => {
 	}
 });
 
-router.get('/tasks', async (req, res) => {
+router.get('/tasks', auth, async (req, res) => {
 	try {
-		const tasks = await Task.find();
+		/* Alternative approach using populate method
+		First, populate tasks object on user */
+		// await req.user.populate('tasks').execPopulate();
+		/* Then send back object */
+		// res.status(200).send(req.user.tasks);
+
+		const tasks = await Task.find({
+			owner: req.user._id
+		});
+
 		res.status(200).send(tasks);
 	} catch (error) {
 		res.status(500).send(error);
 	}
 });
 
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks/:id', auth, async (req, res) => {
 	try {
-		const task = await Task.findById(req.params.id);
+		const task = await Task.findOne({
+			_id: req.params.id,
+			owner: req.user._id
+		});
+
 		if (!task) {
 			return res.status(404).send();
 		}
+
 		res.status(200).send(task);
 	} catch (error) {
 		res.status(500).send(error);
