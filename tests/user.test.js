98529a829d5b3b5aeedc1ce262034f32a29ddd30
Tim Acker <tjacker31@gmail.com>
2019-05-10 23:25:43 -0400

    Tests for logging in existing and non-existing users

--- tests/user.test.js
+++ tests/user.test.js
@@ -15,21 +15,41 @@ const users = {
 		name: `${faker.name.firstName()} ${faker.name.lastName()}`,
 		email: faker.internet.email(),
 		password: faker.internet.password(8)
+	},
+	nonExisting: {
+		name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+		email: faker.internet.email(),
+		password: faker.internet.password(8)
 	}
 };
+
+console.info(users);
 
 // Remove users from the database before testing
 beforeEach(async () => {
 	await User.deleteMany();
+	await new User(users.existing).save();
 });
 
 test('Should sign up a new user', async () => {
 	await request(app)
 		.post('/users')
-		.send({
-			name: 'Tim Acker',
-			email: 'tim@example.com',
-			password: 'Red12345!'
-		})
+		.send(users.new)
 		.expect(201);
+});
+
+test('Should login existing user', async () => {
+	const { email, password } = users.existing;
+	await request(app)
+		.post('/users/login')
+		.send({ email, password })
+		.expect(200);
+});
+
+test('Should not login non-existent user', async () => {
+	const { email, password } = users.nonExisting;
+	await request(app)
+		.post('/users/login')
+		.send({ email, password })
+		.expect(400);
 });
