b41d8b08bf6eb2c903c09e1f2ae4f36f261e4c7f
Tim Acker <tjacker31@gmail.com>
2019-05-10 22:18:21 -0400

    Adds beforeEach method to remove user data from database before testing

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,7 +1,13 @@
-/* global test */
+/* global test beforeEach */
 const request = require('supertest');
 
 const app = require('../src/server');
+const User = require('../src/models/User');
+
+// Remove users from the database before testing
+beforeEach(async () => {
+	await User.deleteMany();
+});
 
 test('Should sign up a new user', async () => {
 	await request(app)
