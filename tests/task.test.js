055b3d39e7afb496463e4703eee7c782692cb1a7
Tim Acker <tjacker31@gmail.com>
2019-05-13 21:38:43 -0400

    Adds tests for creating a task and returning all tasks for a single user

---
+++ tests/task.test.js
@@ -0,0 +1,40 @@
+/* global test beforeEach expect */
+const request = require('supertest');
+
+const app = require('../src/server');
+const Task = require('../src/models/Task');
+const { users, tasks, setupDatabase } = require('./fixtures/data');
+
+console.info(users);
+console.info(tasks);
+
+// Remove then add data in database for testing
+beforeEach(setupDatabase);
+
+test('Should create task for user', async () => {
+	const response = await request(app)
+		.post('/tasks')
+		.set('Authorization', `Bearer ${users.existingOne.tokens[0].token}`)
+		.send({
+			description: tasks.existingOne.taskOne.description
+		})
+		.expect(201);
+
+	// Assert that task was saved to database
+	const task = await Task.findById(response.body._id);
+	expect(task).not.toBeNull();
+
+	// Assert task completed to be false
+	expect(task.completed).toEqual(false);
+});
+
+test('Should get all tasks for user', async () => {
+	const response = await request(app)
+		.get('/tasks')
+		.set('Authorization', `Bearer ${users.existingTwo.tokens[0].token}`)
+		.send()
+		.expect(200);
+
+	// Assert the correct number of tasks received
+	expect(response.body.length).toBe(3);
+});
