3a76182188f3d16d65ddeff8f9b89310fa74dd17
Tim Acker <tjacker31@gmail.com>
2019-05-13 21:36:08 -0400

    Creates addition test data for testing task routes

--- tests/fixtures/data.js
+++ tests/fixtures/data.js
@@ -3,12 +3,15 @@ const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 
 const User = require('../../src/models/User');
+const Task = require('../../src/models/Task');
 
 const ids = {
-	existing: new mongoose.Types.ObjectId()
+	existingOne: new mongoose.Types.ObjectId(),
+	existingTwo: new mongoose.Types.ObjectId()
 };
 const tokens = {
-	existing: jwt.sign({ _id: ids.existing.toString() }, process.env.JWT_SECRET)
+	existingOne: jwt.sign({ _id: ids.existingOne.toString() }, process.env.JWT_SECRET),
+	existingTwo: jwt.sign({ _id: ids.existingTwo.toString() }, process.env.JWT_SECRET)
 };
 const users = {
 	new: {
@@ -16,14 +19,25 @@ const users = {
 		email: faker.internet.email(),
 		password: faker.internet.password(8)
 	},
-	existing: {
-		_id: ids.existing,
+	existingOne: {
+		_id: ids.existingOne,
 		name: `${faker.name.firstName()} ${faker.name.lastName()}`,
 		email: faker.internet.email(),
 		password: faker.internet.password(8),
 		tokens: [
 			{
-				token: tokens.existing
+				token: tokens.existingOne
+			}
+		]
+	},
+	existingTwo: {
+		_id: ids.existingTwo,
+		name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+		email: faker.internet.email(),
+		password: faker.internet.password(8),
+		tokens: [
+			{
+				token: tokens.existingTwo
 			}
 		]
 	},
@@ -33,16 +47,60 @@ const users = {
 		password: faker.internet.password(8)
 	}
 };
+const tasks = {
+	existingOne: {
+		taskOne: {
+			description: faker.lorem.words(rndNumber())
+		},
+		taskTwo: {
+			_id: new mongoose.Types.ObjectId(),
+			description: faker.lorem.words(rndNumber()),
+			completed: true,
+			owner: users.existingOne._id
+		}
+	},
+	existingTwo: {
+		taskOne: {
+			_id: new mongoose.Types.ObjectId(),
+			description: faker.lorem.words(rndNumber()),
+			completed: false,
+			owner: users.existingTwo._id
+		},
+		taskTwo: {
+			_id: new mongoose.Types.ObjectId(),
+			description: faker.lorem.words(rndNumber()),
+			completed: true,
+			owner: users.existingTwo._id
+		},
+		taskThree: {
+			_id: new mongoose.Types.ObjectId(),
+			description: faker.lorem.words(rndNumber()),
+			completed: true,
+			owner: users.existingTwo._id
+		}
+	}
+};
 
 // Remove then add data in database for testing
 const setupDatabase = async () => {
 	await User.deleteMany();
-	await new User(users.existing).save();
+	await Task.deleteMany();
+	await new User(users.existingOne).save();
+	await new User(users.existingTwo).save();
+	await new Task(tasks.existingOne.taskTwo).save();
+	await new Task(tasks.existingTwo.taskOne).save();
+	await new Task(tasks.existingTwo.taskTwo).save();
+	await new Task(tasks.existingTwo.taskThree).save();
 };
+
+function rndNumber() {
+	return Math.floor(Math.random() * 10) + 1;
+}
 
 module.exports = {
 	ids,
 	tokens,
 	users,
+	tasks,
 	setupDatabase
 };
